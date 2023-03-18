dd70db92b3cdf83fcdd4b9e65ace6944f5d41512
Drugsosos <44712637+Drugsosos@users.noreply.github.com>
2023-03-18 20:12:19 +0400

    updated custom module template

--- module_template.py
+++ module_template.py
@@ -1,8 +1,9 @@
+"""Custom module template example"""
 from mac_cleanup import *
 # Do not import any functions at the top level
 
 # Get an instance of Collector
-t = Collector()
+clc = Collector()
 
 
 # Module's name in configuration screen == function name
@@ -10,23 +11,38 @@ def module_example_1():
     # Import anything you need from utils here
     from mac_cleanup.utils import check_exists, cmd
 
+    # check_exists - checks if specified path exists
     if check_exists("~/example/path"):
-        # .msg() is necessary in the start of module, adds new collection in execute_list
-        # IF WASN'T CALLED - commands will be added in last module
-        t.msg("Message you want to see in progress bar")
+        # Open context manager of Collector
+        with clc as t:
+            # message() - sets message to be displayed in the progress bar
+            t.message("Message you want to see in progress bar")
 
-        # .collect() w/o arguments will delete path
-        t.collect("~/example/path")
+            # add() - adds desired module to modules list
+            t.add(
+                # Path - used for deleting paths
+                Path("~/example/path")
+                # with_prompt - calls for user prompt to approve "risky" action
+                # You can specify question in prompt with optional attr "message_"
+                .with_prompt()
+            )
 
-        # cmd() will execute the command and return the stdout & stderr
-        if cmd("echo 1") == "1":
-            # .collect() w/ command=True will execute command and return stdout & stderr
-            t.collect("whoami",
-                      command=True)
+            t.add(
+                Path("~/example/dry_run/file.webm")
+                # dry_run_only - specified path will be counted in dry run, but won't be deleted
+                .dry_run_only()
+            )
 
-        # .collect() w/ dry=True will be counted in dry run but won't be deleted
-        t.collect("~/example/dry_run/file.webm",
-                  dry=True)
+            # cmd() - executes specified command and return stdout only
+            # stderr can be returned with attr "ignore_errors" set to False
+            if cmd("echo 1") == "1":
+                t.add(
+                    # Command - used for executing any command with :func:`mac_cleanup.utils.cmd`
+                    Command("whoami")
+                    .with_prompt("You will see your username. Proceed?")
+                    # with_errors - adds stderr to return of command execution
+                    .with_errors()
+                )
 
 
 # You can create lots of modules in one file
