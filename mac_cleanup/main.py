ab66654d6a2adfefef44809b2961aeeae58aeca4
Drugsosos <44712637+Drugsosos@users.noreply.github.com>
2023-03-24 05:07:38 +0400

    updated main

--- mac_cleanup/main.py
+++ mac_cleanup/main.py
@@ -1,79 +1,73 @@
-from mac_cleanup.collector import _BaseCollector
-from mac_cleanup.core import BaseModule
+from pathlib import Path
+
+from mac_cleanup import args
+from mac_cleanup.core import _Collector
+from mac_cleanup.core_modules import BaseModule
+from mac_cleanup.console import console, print_panel
+from mac_cleanup.config import Config
 from mac_cleanup.utils import cmd, bytes_to_human
-# from mac_cleanup.utils import catch_exception
-from mac_cleanup.console import args, console, print_panel
-from mac_cleanup.config import load_config
-
-t = _BaseCollector()
 
 
-# @catch_exception
-def main() -> None:  # TODO: rewrite
+config_path = Path.home().joinpath(".mac_cleanup_py")
+base_collector = _Collector()
+
+
+def main() -> None:
     # Clear console at the start
     console.clear()
 
-    # Sets custom modules' path if user prompted to and exits
-    if args.modules:
-        from mac_cleanup.config import set_custom_path
+    # Get config
+    config = Config(config_path_=config_path)
 
+    # Sets custom modules' path if user prompted to and exits
+    if args.custom_path:
         # Set custom path
-        set_custom_path()
+        config.set_custom_path()
 
         # Exit
-        raise KeyboardInterrupt
+        config.full_exit(failed=False)
 
-    # Load modules
-    load_config(configuration_needed=args.configure)
-
-    # Exits if configuration was requested
-    if args.configure:  # TODO: move logic here
-        raise KeyboardInterrupt
+    # Check config
+    config(configuration_prompted=args.configure)
 
     def count_free_space(
     ) -> int:
         return int(cmd("df / | tail -1 | awk '{print $4}'"))
 
-    def cleanup(
-    ) -> None:
+    def cleanup() -> None:
         from mac_cleanup.progress import ProgressBar
 
         # Free space before the run
-        oldAvailable = count_free_space()
+        free_space_before = count_free_space()
 
-        # Ask for password input in terminal (sudo -E)
-        # Raises AssertionError if prompt fails
-        assert cmd("sudo -E whoami") == "root"
-
-        for unit in t._execute_list:
+        for unit in base_collector._execute_list:  # noqa
             for module in ProgressBar.wrap_iter(
                     unit.modules,
                     description=unit.message,
-                    transient=True,
                     total=len(unit.modules)
             ):
                 # Fix type hinting
                 module: BaseModule
 
                 # Call for module execution
-                module._execute()
+                module._execute()  # noqa
 
         # Free space after the run
-        newAvailable = count_free_space()
+        free_space_after = count_free_space()
 
         # Print results
         print_panel(
-            text=f"Removed - [success]{bytes_to_human((newAvailable - oldAvailable) * 1024)}",
+            text=f"Removed - [success]{bytes_to_human((free_space_after - free_space_before) * 1024)}",
             title="[info]Success",
         )
 
-    # Straight to clean up if not dry run
-    if not args.dry_run:
-        cleanup()
-    else:
+    # Handle dry runs
+    if args.dry_run:
         from rich.prompt import Confirm
 
-        freed_space = bytes_to_human(t._count_dry())
+        freed_space = bytes_to_human(
+            base_collector._count_dry()  # noqa
+        )
 
         print_panel(
             text=f"Approx [success]{freed_space}[/success] will be cleaned",
@@ -81,21 +75,26 @@ def main() -> None:  # TODO: rewrite
         )
 
         try:
-            if Confirm.ask("Continue?", show_default=False, default="y"):
-                console.clear()
-                cleanup()
-            else:
-                console.clear()
-                console.print("Exiting...")
-
+            continue_cleanup: bool = Confirm.ask("Continue?", show_default=False, default="y")
         # Cyrillic symbols may crash rich.Confirm
         except UnicodeDecodeError:
             console.clear()
             console.print(
-                "Do not enter symbols that can't be decoded to utf-8",
+                "Do not enter symbols that can't be decoded to UTF-8",
                 style="danger",
             )
             console.print("Exiting...")
+            return
+
+        console.clear()
+
+        # Exit if user doesn't want to continue
+        if not continue_cleanup:
+            console.print("Exiting...")
+            return
+
+    # Clean stuff up
+    cleanup()
 
 
 if __name__ == "__main__":
