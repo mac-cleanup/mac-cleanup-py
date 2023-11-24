f4a8b2c8386339c8372737d22659891901f1f998
namnh198 <namnhn98@gmail.com>
2023-11-24 17:05:45 +0700

    add clean bun global cache

--- mac_cleanup/default_modules.py
+++ mac_cleanup/default_modules.py
@@ -336,6 +336,14 @@ def yarn():
             unit.add(Command("yarn cache clean --force"))
             unit.add(Path("~/Library/Caches/yarn").dry_run_only())
 
+def bun():
+    from mac_cleanup.utils import cmd
+
+    if cmd("type 'bun'"):
+        with clc as unit:
+            unit.message("Cleaning up Bun Cache")
+            unit.add(Command(" bun pm cache rm"))
+            unit.add(Path("~/.bun/install/cache").dry_run_only())
 
 def pod():
     from mac_cleanup.utils import cmd
